eafbb338e27366f2f8a3e903a7ff60b12a96e8d5
Ikrar Ansari <85474637+ikrar24@users.noreply.github.com>
2026-05-17 20:45:32 +0530

    Update baseUrl for production environment

--- tools/utils/createToken.js
+++ tools/utils/createToken.js
@@ -1,7 +1,7 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://bostviewers.com" 
-    : "http://localhost:4000" || process.env.NEXT_PUBLIC_BASE_URL ;
+    : "https://apibostviewers.onrender.com" || process.env.NEXT_PUBLIC_BASE_URL ;
 
 export async function createToken() {
   try {
